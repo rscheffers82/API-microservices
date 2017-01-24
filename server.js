edceeca6df726fff8c25abf0b2596ed7dda089b7
Roy Scheffers <r.scheffers@gmail.com>
2017-01-23 23:38:07 -0600

    Add route logic to pick up date details

--- server.js
+++ server.js
@@ -1,13 +1,52 @@
 var express = require('express');
 
-var app = express()
+var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+//-----------------------
+//    Variables
+//-----------------------
+const noDate = {
+  unix: null,
+  human: null
+};
+
+const month = [
+  'jan', 'feb', 'mar', 'apr', 'may', 'june', 'july', 'aug', 'sept', 'oct', 'nov', 'dec',
+  'january','february','march','april','may','june','july','august','september','october','november','december'
+];
+
+//-----------------------
+//    Functions
+//-----------------------
+isValidHumanDate = function(input){
+// Valid: dd-mm-yyyy or Dec | December 1st, 2016
+
+}
+
+serveTime = function(input, type, res) {
+  res.send(input + 'hello');
+}
+
+
+//-----------------------
+//    Routes
+//-----------------------
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+})
+app.get('/:timestamp', function(req, res){
+  const input = req.params.timestamp;
+
+  // determine if input is a unix timestamp
+  if ( !isNaN(input) ) serveTime(input, 'unix', res);
+  else if ( isValidHumanDate(input) ) serveTime(input, 'human', res);
+  else res.send(noDate);
+
+
+
 })
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, function () {
-  console.log('Example app listening on port ', PORT);
-})
+app.listen(PORT);
+console.log('Example app listening on port ', PORT);
