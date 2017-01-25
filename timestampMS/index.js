06b9671500d562f5369f6c8e5dd32b50a315d26e
Roy Scheffers <r.scheffers@gmail.com>
2017-01-25 11:38:22 -0600

    add some basic styles

--- timestampMS/index.js
+++ timestampMS/index.js
@@ -4,25 +4,25 @@ var checkForDate = function(rawInputOrigin) {
 
   rawInput = rawInputOrigin;
   rawInput = rawInput.replace(/(st|nd|rd|th)/g,'');   // remove st, nd, rd, th from the day if provided
-  const unixDate = Date.parse(rawInput);              // try to convert a human date to a unix timestamp
-  const humanDateFormat = 'MMM D YYYY';
+  const unixDate = Date.parse(rawInput);              // try to convert a natural date to a unix timestamp
+  const naturalDateFormat = 'MMM D YYYY';
   var output = {};
 
   if ( !isNaN(rawInput) ) {
     // a valid unix timestamp was provided
-    const humanDate = moment.unix(rawInput).format(humanDateFormat);
+    const naturalDate = moment.unix(rawInput).format(naturalDateFormat);
 
     output.unix = rawInput;
-    output.human = humanDate;
+    output.natural = naturalDate;
   }
   else if ( !isNaN(unixDate) ) {
-    // rawInput contains a valid human date and was successfully converted to a unix timestamp
+    // rawInput contains a valid natural date and was successfully converted to a unix timestamp
     output.unix = unixDate / 1000;              // /1000 to convert from ms to sec
-    output.human = rawInputOrigin;              // return the original input provided
+    output.natural = rawInputOrigin;              // return the original input provided
   }
   else {
     output.unix = null;
-    output.human = null;
+    output.natural = null;
   }
 
   return output;
