a6fb089ed6018bd0837eb78288175d68847c346f
Roy Scheffers <r.scheffers@gmail.com>
2017-02-01 23:27:11 -0600

    initial commit for request header parser API

--- server.js
+++ server.js
@@ -24,13 +24,25 @@ app.get('/', function(req, res){
 })
 
 // -- timestamp API -- \\
-app.use( '/timestamp', express.static( path.join(__dirname + '/timestamp-ms/public') ) );   // automatically serve static files in the timestamp public file, in this case index.html
+app.use( '/timestamp', express.static( path.join(__dirname + '/timestamp-ms/public') ) );   // automatically serve static files in the timestamp public folder, in this case index.html
 app.get('/timestamp/:timestamp', function(req, res){
   const rawInput = req.params.timestamp;         // catch the input off of the url, either text or unix timestamp
   res.send( checkForDate(rawInput) );
 });
 
-// -- Request Header Parser -- \\
+// -- Request Header Parser API -- \\
+
+app.get('/whoami', function(req, res){
+  const ip = req.connection.remoteAddress;
+  res.send({ ip });
+
+
+  // {      -- expected output
+  //   "ipaddress": "73.110.16.140",
+  //   "language": "en-US",
+  //   "software": "Windows NT 10.0; WOW64; rv:53.0"
+  // }
+}
 
 
 
@@ -38,6 +50,7 @@ app.get('/timestamp/:timestamp', function(req, res){
 //-----------------------
 //    Resources
 //-----------------------
+// *** Timestamp API
 // http://momentjs.com/docs/#/parsing/string-format/
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/parse#Using_Date.parse()
