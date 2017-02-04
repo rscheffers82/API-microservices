bdd7c7b3860af2ef9380c73de489c28b350bd384
Roy Scheffers <r.scheffers@gmail.com>
2017-02-03 22:12:58 -0600

    style json output

--- request-header/index.js
+++ request-header/index.js
@@ -4,19 +4,18 @@ const os = require('os');
 var headerAPI = function(req) {
   var header = {};
 
-  header.ipaddress = req.ip;
+  // header.ipaddress = req.ip;
+  // this will be more accurate and less poluted than the above which outputs on heroku "::ffff:10.61.238.152",
+  header.ipAddress = req.headers['x-forwarded-for'] ||
+     req.connection.remoteAddress ||
+     req.socket.remoteAddress ||
+     req.connection.socket.remoteAddress;
+
   header.language = accepts(req).languages()[0];
   header.software = os.type() + ' ' +
-                    os.release() + ' ' +
+                    os.release().slice(0,4) + ' ' +
                     os.arch();
-
   return header;
 }
 
 module.exports = headerAPI;
-
-// {      -- expected output
-//   "ipaddress": "73.110.16.140",
-//   "language": "en-US",
-//   "software": "Windows NT 10.0; WOW64; rv:53.0"
-// }
