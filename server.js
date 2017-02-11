f0ab8ab04b49a935b51ee2e87d151d92bb18fce5
Roy Scheffers <r.scheffers@gmail.com>
2017-02-10 22:33:46 -0500

    update API with pseudo code and references

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const path = require('path');
 
 const checkForDate = require('./timestamp-ms/API');
 const headerAPI = require('./request-header');
+const shorten = require('./url-shortener')
 
-const config = require ('./config');
 //-----------------------
 //    Express settings
 //-----------------------
@@ -39,12 +39,9 @@ app.get('/whoami', function(req, res){
 
 // -- URL Shortener API -- \\
 app.use( '/shorten', express.static( path.join(__dirname + '/url-shortener/public') ) );   // automatically serve static files in the timestamp public folder, in this case index.html
-app.get('/short', function(req, res){
-  // res.json( headerAPI(req) );
-
-  // var url = process.env.MONGOLAB_URI;
-  var url = config.MONGOLAB_URI;
-  res.send('shorten me...' + url);
+app.get('/short/:url', function(req, res){
+  var longUrl = req.params.url;
+  res.json( shorten(longUrl) );
 });
 
 
--- url-shortener/index.js
+++ url-shortener/index.js
@@ -1,3 +1,23 @@
-var shorten = function(input) {
+const config = require ('../config');
+
+var shorten = function(longUrl) {
+  // var mongoUrl = process.env.MONGOLAB_URI;
+  var mongoUrl = config.MONGOLAB_URI;
+
+  // Shorten URL
+
+  // DB structure: Primary key | shortcode | longURL
+  // shortcode: 0 to 9 and a to z
+
+  // Store in the DB
+  // Use the below for a unique identifier
+  // https://www.npmjs.com/package/mongoose-shortid
+
+  // return json
+
+
+
+  return mongoUrl;
+}
 
 module.exports = shorten;
