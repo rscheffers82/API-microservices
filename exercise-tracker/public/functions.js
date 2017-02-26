b0197c8c9de40eca4c857ff39a09abc47ff9cabb
Roy Scheffers <r.scheffers@gmail.com>
2017-02-26 17:40:57 -0500

    Add materialCSS to the project and initial style of the main page

---
+++ exercise-tracker/public/functions.js
@@ -0,0 +1,11 @@
+
+$( function() {
+  $( "#datepicker" ).datepicker({ dateFormat: "mm-dd-yy" });
+  $( "#datepicker" ).datepicker( "option", "showAnim", "slideDown" );
+  $( "#datepicker" ).datepicker();
+  })();
+
+// $('.today').on('click', function(e) {
+//   e.preventDefault();
+//   $( "#datepicker" ).datepicker( "setDate", "10-12-2012" );
+// });
