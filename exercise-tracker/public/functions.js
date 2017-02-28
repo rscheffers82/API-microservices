ad56bc5688ec41fb513bbdac7a841ec533c71f04
Roy Scheffers <r.scheffers@gmail.com>
2017-02-27 23:14:08 -0500

    add datepicker to all date fields

--- exercise-tracker/public/functions.js
+++ exercise-tracker/public/functions.js
@@ -1,8 +1,16 @@
 
 $( function() {
-  $( '#datepicker' ).datepicker({ dateFormat: 'mm-dd-yy' });
-  $( '#datepicker' ).datepicker( 'option', 'showAnim', 'slideDown' );
-  $( '#datepicker' ).datepicker();
+  $( '#add-datepicker' ).datepicker({ dateFormat: 'mm-dd-yy' });
+  $( '#add-datepicker' ).datepicker( 'option', 'showAnim', 'slideDown' );
+  $( '#add-datepicker' ).datepicker();
+
+  $( '#from-datepicker' ).datepicker({ dateFormat: 'mm-dd-yy' });
+  $( '#from-datepicker' ).datepicker( 'option', 'showAnim', 'slideDown' );
+  $( '#from-datepicker' ).datepicker();
+
+  $( '#to-datepicker' ).datepicker({ dateFormat: 'mm-dd-yy' });
+  $( '#to-datepicker' ).datepicker( 'option', 'showAnim', 'slideDown' );
+  $( '#to-datepicker' ).datepicker();
 })();
 
 // $('.today').on('click', function(e) {
