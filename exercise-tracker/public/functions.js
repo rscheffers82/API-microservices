33f12e798ade41c985469c256104fc0ac1fd8140
Roy Scheffers <r.scheffers@gmail.com>
2017-02-28 22:43:10 -0500

    add nav side menu

--- exercise-tracker/public/functions.js
+++ exercise-tracker/public/functions.js
@@ -10,3 +10,16 @@ const dateOptions = {
 $('.add-datepicker').pickadate(dateOptions);
 $('.from-datepicker').pickadate(dateOptions);
 $('.to-datepicker').pickadate(dateOptions);
+
+$('.button-collapse').sideNav({
+    menuWidth: 300, // Default is 300
+    edge: 'left', // Choose the horizontal origin
+    closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    draggable: true // Choose whether you can drag to open on touch screens
+  }
+);
+
+  // // Show sideNav
+  // $('.button-collapse').sideNav('show');
+  // // Hide sideNav
+  // $('.button-collapse').sideNav('hide');
