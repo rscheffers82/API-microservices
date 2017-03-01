eb160fe1c538689a0bdfb1c152d7eea3fc023534
Roy Scheffers <r.scheffers@gmail.com>
2017-02-28 23:52:25 -0500

    style side menu

--- exercise-tracker/public/functions.js
+++ exercise-tracker/public/functions.js
@@ -20,6 +20,6 @@ $('.button-collapse').sideNav({
 );
 
   // // Show sideNav
-  // $('.button-collapse').sideNav('show');
+  $('.button-collapse').sideNav('show');
   // // Hide sideNav
   // $('.button-collapse').sideNav('hide');
