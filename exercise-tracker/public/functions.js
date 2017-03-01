807f7a4164ec40d5fc33701646c4bb795023ee34
Roy Scheffers <r.scheffers@gmail.com>
2017-02-28 22:26:09 -0500

    Update styles from MaterialCSS to MaterializeCSS

--- exercise-tracker/public/functions.js
+++ exercise-tracker/public/functions.js
@@ -1,46 +1,12 @@
-// var mc = require('materialcss');
+const dateOptions = {
+  selectMonths: true, // Creates a dropdown to control month
+  selectYears: 15, // Creates a dropdown of 15 years to control year
+  selectYears: true,
+  selectMonths: true,
+  format: 'd mmm, yyyy',
+  formatSubmit: 'dd-mm-yyyy',
+};
 
-
-$('#main-menu').click(function() {
-  $('body > .mc-navigation').removeClass('closed');
-});
-
-$('#close-main-menu').click(function() {
-  $('body > .mc-navigation').addClass('closed');
-});
-
-$( function() {
-  $( '#add-datepicker' ).datepicker({
-    dateFormat: 'mm-dd-yy',
-    onSelect: function() {
-      $('#add-label').addClass('mc-label mc-completed-label');
-    }
-  });
-  $( '#add-datepicker' ).datepicker( 'option', 'showAnim', 'slideDown' );
-  $( '#add-datepicker' ).datepicker();
-
-  $( '#from-datepicker' ).datepicker({
-    dateFormat: 'mm-dd-yy',
-    onSelect: function() {
-      $('#from-label').addClass('mc-label mc-completed-label');
-    }
-  });
-  $( '#from-datepicker' ).datepicker( 'option', 'showAnim', 'slideDown' );
-  $( '#from-datepicker' ).datepicker();
-
-  $( '#to-datepicker' ).datepicker({
-    dateFormat: 'mm-dd-yy',
-    onSelect: function() {
-      $('#to-label').addClass('mc-label mc-completed-label');
-    }
-  });
-  $( '#to-datepicker' ).datepicker( 'option', 'showAnim', 'slideDown' );
-  $( '#to-datepicker' ).datepicker();
-
-  // mc-label mc-completed-label
-})();
-
-// $('.today').on('click', function(e) {
-//   e.preventDefault();
-//   $( '#datepicker' ).datepicker( 'setDate', '10-12-2012' );
-// });
+$('.add-datepicker').pickadate(dateOptions);
+$('.from-datepicker').pickadate(dateOptions);
+$('.to-datepicker').pickadate(dateOptions);
