1a59d82572c52a19f818077d575e883e14a58506
Roy Scheffers <r.scheffers@gmail.com>
2018-08-17 12:32:08 +0200

    add support for usePushEach in Mongo

--- exercise-tracker/model/user.js
+++ exercise-tracker/model/user.js
@@ -1,7 +1,7 @@
 const ShortId = require('mongoose-shortid-nodeps');
 const mongoose = require('mongoose');
+mongoose.plugin(schema => { schema.options.usePushEach = true });       // Needed since Mongo 3.6.2, The $pushAll operator is no longer supported in earlier versions
 const Schema = mongoose.Schema;
-
 
 // the model to work with
 const UserSchema = new Schema({
